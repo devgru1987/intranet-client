b8e31aff19622794a4956dba561dbabf15de069b
devgru1987 <hugoace2011@gmail.com>
2025-01-26 16:59:51 +0300

    ADD: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import LogoBanner from './assets/header.svg'
+import IthubaUganda from './assets/ithuba_uganda.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="container">
+      <header>
+        <div className="flex-row center-content-horizontal">
+          <div className="flex-row percentage-width-85 admin-login">
+            <a>Admin Login</a>
+          </div>
+        </div>
+
+        <div id="main-logo" className="flex-row center-content-horizontal">
+          <div id="ithuba-logo" className="percentage-width-80">
+            <img src={LogoBanner} alt='banner' />
+          </div>
+        </div>
+
+        <div className="flex-row center-content-horizontal">
+          <div className="flex-row percentage-width-80 header-nav"> 
+            <h3>Welcome to <span className="uppercase">ithuba uganda</span></h3>
+            <ul className="flex-row nav-list">
+              <li>Home</li>
+              <li>Business Resources</li>
+              <li>Employee center</li>
+              <li>News</li>
+              <li>Career</li>
+              <li className="uppercase">faqs</li>
+            </ul>
+          </div>
+        </div>
+      </header>
+
+      <section id="hero-unit">
+
+      </section>
+
+
+      
+      
+    </div>
   )
 }
 
