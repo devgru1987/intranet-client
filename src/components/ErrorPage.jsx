627ffd4a2d3c04fbb96dfaa96cbfdf2857a10101
devgru1987 <hugoace2011@gmail.com>
2025-02-08 21:59:07 +0300

    ADD: Error page

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,25 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
+
+
+
+/*
+Note that useRouteError provides the error that was thrown. When the user navigates to routes 
+that don't exist you'll get an error response with a "Not Found" statusText. We'll see some other 
+errors later in the tutorial and discuss them more.
+
+*/ 
