bdd52145d10695bba247abe54df589c96a631d77
devgru1987 <hugoace2011@gmail.com>
2025-02-10 17:08:54 +0300

    ADD: users Calender view

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,12 +1,30 @@
+/* https://www.npmjs.com/package/react-big-calendar */ 
+
 import Navigation from "./Navigation"
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
+const localizer = momentLocalizer(moment)
 
 const Calender = () => {
-  
+  const myEventsList = []
 
   return (
     <div>
       <section className="main-navigation">
         <Navigation />
+      </section>
+
+      <section className="flex-row justify-center">
+        <div className="percentage-width-80 margin-top-btm-70">
+          <Calendar
+            localizer={localizer}
+            events={myEventsList}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 500 }} />
+        </div>
       </section>
     </div>
   )
