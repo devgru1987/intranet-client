74f1c5aa410f313019ce35e2b9449c8a2b49242d
devgru1987 <hugoace2011@gmail.com>
2025-02-10 14:13:09 +0300

    ADD: photo gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,22 @@
 import Navigation from "./Navigation"
+import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from "react-image-gallery";
+
+const images = [
+ {
+   original: "https://picsum.photos/id/1018/1000/600/",
+   thumbnail: "https://picsum.photos/id/1018/250/150/",
+ },
+ {
+   original: "https://picsum.photos/id/1015/1000/600/",
+   thumbnail: "https://picsum.photos/id/1015/250/150/",
+ },
+ {
+   original: "https://picsum.photos/id/1019/1000/600/",
+   thumbnail: "https://picsum.photos/id/1019/250/150/",
+ },
+];
+
 
 const Gallery = () => {
 
@@ -7,7 +25,10 @@ const Gallery = () => {
        <section className="main-navigation">
        <Navigation />
        </section>
-     
+
+       <section>
+       <ImageGallery items={images} />
+       </section>
     </div>
   )
 }
