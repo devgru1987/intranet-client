6b8a5b7f5a392a7e052ad3d4bacc810ec5a086a2
devgru1987 <hugoace2011@gmail.com>
2025-02-12 23:16:41 +0300

    ADD: Date selectors and event input field

--- src/components/AdminCalender.jsx
+++ src/components/AdminCalender.jsx
@@ -3,10 +3,24 @@
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+//https://www.npmjs.com/package/react-date-picker
+import DatePicker from 'react-date-picker'
 
 const localizer = momentLocalizer(moment)
 
 const AdminCalender = () => {
+
+ const startDate = (e) => {
+   console.log(e.target.value)
+ }
+
+ const startEnd = (e) => {
+  console.log(e.target.value)
+}
+
+const saveEvent = () => {
+
+}
 
  const myEventsList = []
 
@@ -21,11 +35,11 @@ const AdminCalender = () => {
          style={{ height: 600 }} />
      </div>
 
-     <div className=" percentage-width-90 height-30 margin-btm-20 flex-row justify-space-btn">
-     <input placeholder='Event Title' className="relative-width-50 pad-all-side event-input" />
-     <div className="relative-width-15 dp-border">start date</div>
-     <div className="relative-width-15 dp-border">end date</div>
-     <div className="relative-width-10 dp-border">Save</div>
+     <div className=" percentage-width-90 height-40 margin-btm-20 flex-row justify-space-btn">
+       <input placeholder='Event Title' className="relative-width-50 pad-all-side event-input" />
+       <div className="relative-width-15">Start: <input aria-label="Date" type="date" className="height-40" onChange={(e) =>  startDate(e) }/></div>
+       <div className="relative-width-15">End: <input aria-label="Date" type="date" className="height-40" onChange={(e) =>  startEnd(e)}/></div>
+       <div className="relative-width-10"><button className="cal-btn" onClick={() => saveEvent()}>Save</button></div>
      </div>
     </section>
  )
